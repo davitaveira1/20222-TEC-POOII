85f99bb1f70302460f9c3c219cd1a9f9ab27f769
Davi Taveira <davitaveira@gmail.com>
2022-08-23 16:48:22 -0300

    Aula 03

---
+++ TEC-POOII-producao-Aula03/src/Ex04/quadrado.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ex04;
+
+/**
+ *
+ * @author Davi
+ */
+public class quadrado {
+    
+    double lado;
+    double area;
+    double perimetro;
+
+    public quadrado(double lado) {
+        this.lado = lado;
+    }
+    
+    
+    
+}

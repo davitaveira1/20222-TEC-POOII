bd055fd3a619f2d9bff76fbd98525ee0b57630e4
Davi Taveira <davitaveira@gmail.com>
2022-11-08 08:04:05 -0300

    aula-10-0811-p2

--- TEC-POOII-producao-Aula10/src/Exemplos/Ex02.java
+++ TEC-POOII-producao-Aula10/src/Exemplos/Ex02.java
@@ -7,43 +7,60 @@ package Exemplos;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Davi
  */
 public class Ex02 {
-    
-    Connection conn=null;
-    PreparedStatement pst=null;
-    Statement st=null;
-    
+
+    Connection conn = null;
+    PreparedStatement pst = null;
+    Statement st = null;
+
     //método estático
-    public void alterar(){
-        
+    public void alterar() {
+
         try {
-            
+
             conn = db.conexao.getConnection();
             st = conn.createStatement();
             st.executeUpdate("update alunos set"
                     + " nome='Davi Taveira Alencar'"
                     + " where codigo=1");
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"Erro: "+ e);
+        }
+
+    }
+
+    //método dinâmico
+    public void alterar(String nome, int codigo) {
+        conn = db.conexao.getConnection();
+        try {
+            pst = conn.prepareStatement("update"
+                    + " alunos set nome=?"
+                    + " where codigo=?");
+            
+            pst.setString(1, nome);
+            pst.setInt(2, codigo);
+            
+            pst.executeUpdate();
             
         } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"Erro: "+ e);
         }
-        
+
     }
-    //método dinâmico
-    public void alterar(String nome,int codigo){
-        conn = db.conexao.getConnection();
-        //pst = conn.pre
-    }
-    
+
     public static void main(String[] args) {
-        
+
         Ex02 e = new Ex02();
-        e.alterar();
-        
+        //e.alterar();
+        e.alterar("Daniel Taveira Alencar", 7);
+
     }
-    
+
 }
